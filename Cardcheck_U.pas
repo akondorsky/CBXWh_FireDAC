99c0c67b9d4663a18b398360afa198e8f8d937f5
akondorsky@gmail.com <akondorsky>
2023-12-14 03:46:14 +0200

    Firsy commit

---
+++ Cardcheck_U.pas
@@ -0,0 +1,16 @@
+unit Cardcheck_U;
+
+interface
+uses
+  Variants, Classes,
+  ActiveX, ComObj, oleserver, StdCtrls, Controls, ExtCtrls,
+  Contactless,ESDK2Lib_TLB;
+
+type
+  BSTRArray = Array of PWideChar;
+  ByteArray = Array of Byte;
+  PByteArray = ^ByteArray;
+
+implementation
+
+end.
